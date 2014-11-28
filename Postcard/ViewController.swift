dd267815a8cb1448735ea974a7732a08fdced001
HHumbert <Will@Athena.local>
2014-11-28 11:57:08 +0800

    1sr Challenge

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var nameLabel: UILabel!
    @IBOutlet weak var messageLabel: UILabel!
    @IBOutlet weak var enterNameTextField: UITextField!
    @IBOutlet weak var enterMessageTextField: UITextField!
@@ -27,7 +28,12 @@ class ViewController: UIViewController {
 
 
    @IBAction func sendMailButtonPressed (sender: UIButton) {
-      \\ GitHub update test
+      nameLabel.hidden = false
+      nameLabel.text = enterNameTextField.text
+      nameLabel.textColor = UIColor.blueColor()
+      enterNameTextField.text = ""
+      enterNameTextField.resignFirstResponder()
+
       messageLabel.hidden = false
       messageLabel.text = enterMessageTextField.text
       messageLabel.textColor = UIColor.redColor()
